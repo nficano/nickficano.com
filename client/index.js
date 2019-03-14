5305a04b7c9c1db8e6c1a213f416420bb5217201
Nick Ficano <nficano@gmail.com>
2019-03-14 16:53:52 -0400

    cleanup

--- client/index.js
+++ client/index.js
@@ -1,9 +1,5 @@
 import './scss/main.scss';
 
-if ('ontouchstart' in document.documentElement) {
-  document.addEventListener('touchstart', onTouchStart, {passive: true});
-}
-
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
     .register(`service-worker.js?${Math.random()}`)
