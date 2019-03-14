952d92dbb842befe60db625acf2f6605dd4edc02
Nick Ficano <nficano@gmail.com>
2019-03-14 12:26:57 -0400

    use passive listeners to improve scrolling performance

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import './scss/main.scss';
 
+document.addEventListener('touchstart', onTouchStart, {passive: true});
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
     .register(`service-worker.js?${Math.random()}`)
