265f8efcade14204bc74998d53f4095e5dade08d
Nick Ficano <nficano@gmail.com>
2019-03-18 06:44:30 -0400

    height fix

--- client/index.js
+++ client/index.js
@@ -1,5 +1,16 @@
 import './scss/main.scss';
 
+const correctViewpointHeight = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+window.addEventListener('resize', () => {
+  correctViewpointHeight();
+});
+
+correctViewpointHeight();
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
     .register(`service-worker.js?${Math.random()}`)
