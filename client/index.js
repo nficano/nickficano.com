05b76d7e85e587630fe0b31f8af9fb7cb2ae6e24
Nick Ficano <nficano@gmail.com>
2019-03-22 06:15:45 -0400

    fix for pwa content not centered

--- client/index.js
+++ client/index.js
@@ -1,24 +1,27 @@
 import './scss/main.scss';
 
 const correctViewpointHeight = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  if (window.location.search.indexOf('mode')) {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 };
 
-window.addEventListener('resize', () => {
+const init = () => {
+  window.addEventListener('resize', () => correctViewpointHeight());
   correctViewpointHeight();
-});
 
-correctViewpointHeight();
-
-if ('serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     navigator.serviceWorker
     .register(`service-worker.js?${Math.random()}`)
     .then(function(registration) {
-        console.log('Service Worker Registered');
+        console.log('🚀  Service Worker Registered');
         return registration;
     })
     .catch(function(err) {
         console.error('Unable to register service worker.', err);
     });
-}
+  }
+};
+
+init();
