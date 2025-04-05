aae208aee6de4737807983c373dbc0dfb499f750
Nick Ficano <nficano@gmail.com>
2025-04-04 23:01:31 -0400

    goodnight

---
+++ components/Desktop.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="min-h-screen flex flex-col justify-center items-center overflow-hidden relative"
+  >
+    <div class="absolute inset-0 bg-black/20 backdrop-blur-sm md:hidden"></div>
+    <TextHover text="Hello" />
+    <MacDock />
+  </div>
+</template>
+
+<script setup>
+import MacDock from "~/components/MacDock.vue";
+import TextHover from "~/components/TextHover.vue";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,14 @@
 <template>
-  <div
-    class="min-h-screen flex flex-col justify-center items-center overflow-hidden relative"
-  >
-    <div class="absolute inset-0 bg-black/20 backdrop-blur-sm md:hidden"></div>
-    <TextHover text="Hello" />
-    <MacDock />
+  <div>
+    <client-only>
+      <Desktop v-if="!isMobile" />
+      <Mobile v-else />
+    </client-only>
   </div>
 </template>
 
 <script setup>
 import { useBreakpoints, breakpointsTailwind } from "@vueuse/core";
-import MacDock from "~/components/MacDock.vue";
-import TextHover from "~/components/TextHover.vue";
 
 const breakpoints = useBreakpoints(breakpointsTailwind);
 const isMobile = breakpoints.smaller("md");
